d839ace35fdfc492264d5e4f910a20f26a2acb11
Uni <1025687605@qq.com>
2022-11-14 10:03:53 +0800

    feat: add

---
+++ release.config.js
@@ -0,0 +1,22 @@
+module.exports = {
+  repositoryUrl: 'https://github.com/',
+  branches: [
+    { name: 'main', channel: 'latest' },
+    {
+      name: 'qa/latest',
+      channel: 'canary',
+      prerelease: 'beta'
+    }
+  ],
+  plugins: [
+    '@semantic-release/commit-analyzer',
+    '@semantic-release/release-notes-generator',
+    [
+      '@semantic-release/exec',
+      {
+        prepareCmd: 'yarn build'
+      }
+    ],
+    '@semantic-release/git'
+  ]
+}
